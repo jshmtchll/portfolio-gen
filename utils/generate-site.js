366b618254ddcada46f30a417dd764fd7a811f47
josh mitchell <jshmtchll@gmail.com>
2021-04-08 13:03:34 -0500

    added generate site and promise to catch errors

---
+++ utils/generate-site.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            //if theres an error, reject the Promise and send an error to the promis's `.catch()` method
+            if (err) {
+                reject(err);
+                //return out of the function here to make sure promise doesn't accendentially execute the resolve( function as well)
+                return;
+            }
+
+            //if everything went well resolve the promise and send the successful data to the .then() method
+            resolve({
+                ok: true,
+                message: 'file created'
+            });
+        });
+    });
+};
+
+const copyFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'file created'
+            });
+        });
+    });
+};
+
+module.exports = {
+    writeFile: writeFile,
+    copyFile: copyFile
+};
